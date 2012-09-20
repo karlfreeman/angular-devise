0312872714c1de2f23fbe3721391a5cae653c0d0
Karl Freeman <karlfreeman@gmail.com>
2012-09-20 19:35:59 +0100

    rails new

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'https://rubygems.org'
+
+gem 'rails',  '3.2.8'
+gem 'devise', '2.1.0'
+gem 'sqlite3'
+gem 'jbuilder'
+gem 'jquery-rails'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier',     '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularDevise::Application.config.secret_token = 'efe21c14e00beac718f211fb59f5b15638879db40f7c727f5c597c9d1563fa00d41aa811bce04f1d47ceb919dfc88e651fd39f4eeb9b1203469ed5a2edf50e56'
