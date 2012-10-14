0abebab955d3b6da8c6c6b156c409323d1f244c2
Karl Freeman <karlfreeman@gmail.com>
2012-10-14 01:02:44 +0100

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,9 @@ module AngularDevise
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
 
+    #
+    config.assets.initialize_on_precompile = false
+
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
