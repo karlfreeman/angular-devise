9a52d9f8f7dd8894d1e1925d183aa2b0b52cc3b0
Karl Freeman <karlfreeman@gmail.com>
2013-03-26 13:06:00 +0000

    disable for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ AngularDevise::Application.configure do
   config.assets.compress = true
 
   # Compress, but not to hard
-  config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }
+  # config.assets.js_compressor = Sprockets::LazyCompressor.new { Uglifier.new(:mangle => false) }
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = false
